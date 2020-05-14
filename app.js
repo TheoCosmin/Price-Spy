dae9ba83b0d7c15ccbdad5ca8273494b2cfd963e
Ispas Valentin <51799997+ispasvalentin22@users.noreply.github.com>
2020-05-14 19:39:54 +0300

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile("./index.html", { root: __dirname });
+});
+
+app.post("/", (req, res) => {
+  res.send("You can post to this endpoint...");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
